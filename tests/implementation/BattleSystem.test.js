e68f43dac0e433b185e30e42f1dd35d423b05030
T.I.OAK <tioak.puz@gmail.com>
2026-08-28 22:20:40 +0900

    tune: balance battle tilt and recovery

--- tests/implementation/BattleSystem.test.js
+++ tests/implementation/BattleSystem.test.js
@@ -275,8 +275,8 @@ test('damage leaves a random knockback tilt that each action gradually restores'
   battle.applyDamage(null, target, 'sword', 1);
   battle.updateActor(actor, [], 1000);
 
-  assert.equal(target.chip.tilt, -Math.PI / 48);
-  assert.equal(actor.chip.tilt, -0.4 + Math.PI / 12);
+  assert.equal(target.chip.tilt, -Math.PI / 18);
+  assert.equal(actor.chip.tilt, -0.4 + Math.PI / 24);
 });
 
 test('lightning propagates only through contiguous opponent slots', () => {
